3212923bed6fb33505817967146ab08ce91db56c
Anandh Ravindran <ARavindran@walmartlabs.com>
2016-02-15 00:15:05 +0530

    Hack to send unicast to server instead of mulitcast

--- example.sh
+++ example.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 java -jar ./target/scala-2.11/diffy-server.jar \
--candidate='localhost:9200' \
--master.primary='localhost:9000' \
--master.secondary='localhost:9100' \
+-candidate='192.168.99.100:9881' \
+-master.primary='192.168.99.100:9882' \
+-master.secondary='192.168.99.100:9883' \
 -service.protocol='http' \
 -serviceName='My Service' \
 -proxy.port=:8880 \
 -admin.port=:8881 \
 -http.port=:8888 \
 -rootUrl='localhost:8888'
+
--- src/main/scala/com/twitter/diffy/proxy/DifferenceProxy.scala
+++ src/main/scala/com/twitter/diffy/proxy/DifferenceProxy.scala
@@ -16,8 +16,7 @@ object DifferenceProxyModule extends TwitterModule {
     settings: Settings,
     collector: InMemoryDifferenceCollector,
     joinedDifferences: JoinedDifferences,
-    analyzer: DifferenceAnalyzer
-  ): DifferenceProxy =
+    analyzer: DifferenceAnalyzer): DifferenceProxy =
     settings.protocol match {
       case "thrift" => ThriftDifferenceProxy(settings, collector, joinedDifferences, analyzer)
       case "http" => SimpleHttpDifferenceProxy(settings, collector, joinedDifferences, analyzer)
@@ -48,7 +47,7 @@ trait DifferenceProxy {
 
   // Clients for services
   val candidate = serviceFactory(settings.candidate.path, "candidate")
-  val primary   = serviceFactory(settings.primary.path, "primary")
+  val primary = serviceFactory(settings.primary.path, "primary")
   val secondary = serviceFactory(settings.secondary.path, "secondary")
 
   val collector: InMemoryDifferenceCollector
@@ -57,39 +56,82 @@ trait DifferenceProxy {
 
   val analyzer: DifferenceAnalyzer
 
-  private[this] lazy val multicastHandler =
-    new SequentialMulticastService(Seq(primary.client, candidate.client, secondary.client))
+  private[this] lazy val multicastHandlerPrimary =
+    new SequentialMulticastService(Seq(primary.client))
+
+  private[this] lazy val multicastHandlerCandidate =
+    new SequentialMulticastService(Seq(candidate.client))
+
+  private[this] lazy val multicastHandlerSecondary =
+    new SequentialMulticastService(Seq(secondary.client))
+  var requestNo: Int = 0
+
+  var rawResponses: Future[Seq[Try[Rep]]] = Future.Nil
+
+  var rawResponsesPrimary: Future[Seq[Try[Rep]]] = Future.Nil
+
+  var rawResponsesCandidate: Future[Seq[Try[Rep]]] = Future.Nil
+
+  var rawResponsesSecondary: Future[Seq[Try[Rep]]] = Future.Nil
 
   def proxy = new Service[Req, Rep] {
     override def apply(req: Req): Future[Rep] = {
-      val rawResponses =
-        multicastHandler(req) respond {
+      var currentResponse: Future[Seq[Try[Rep]]] = Future.Nil
+      if (requestNo == 0) {
+        rawResponsesPrimary = multicastHandlerPrimary(req) respond {
           case Return(_) => log.debug("success networking")
           case Throw(t) => log.debug(t, "error networking")
         }
-
-      val responses: Future[Seq[Message]] =
-        rawResponses flatMap { reps =>
-          Future.collect(reps map liftResponse) respond {
-            case Return(rs) =>
-              log.debug(s"success lifting ${rs.head.endpoint}")
-
-            case Throw(t) => log.debug(t, "error lifting")
+        log.info("Raw response : " + rawResponsesPrimary.get().toString())
+        currentResponse = rawResponsesPrimary
+      } else if (requestNo == 1) {
+        rawResponsesCandidate =
+          multicastHandlerCandidate(req) respond {
+            case Return(_) => log.debug("success networking")
+            case Throw(t) => log.debug(t, "error networking")
           }
-        }
-
-      responses foreach {
-        case Seq(primaryResponse, candidateResponse, secondaryResponse) =>
-          liftRequest(req) respond {
-            case Return(m) =>
-              log.debug(s"success lifting request for ${m.endpoint}")
-
-            case Throw(t) => log.debug(t, "error lifting request")
-          } foreach { req =>
-            analyzer(req, candidateResponse, primaryResponse, secondaryResponse)
+        log.info("Raw response : " + rawResponsesCandidate.get().toString())
+        currentResponse = rawResponsesCandidate
+      } else if (requestNo == 2) {
+        rawResponsesSecondary =
+          multicastHandlerSecondary(req) respond {
+            case Return(_) => log.debug("success networking")
+            case Throw(t) => log.debug(t, "error networking")
           }
+        log.info("Raw response : " + rawResponsesSecondary.get().toString())
+        currentResponse = rawResponsesSecondary
       }
 
+      if (requestNo == 2) {
+        rawResponses = Future.value(Seq(rawResponsesPrimary.get().head, rawResponsesCandidate.get().head, rawResponsesSecondary.get().head))
+        log.info("Raw response : " + rawResponses.get().toString())
+        val responses: Future[Seq[Message]] =
+          rawResponses flatMap { reps =>
+            Future.collect(reps map liftResponse) respond {
+              case Return(rs) =>
+                log.debug(s"success lifting ${rs.head.endpoint}")
+
+              case Throw(t) => log.debug(t, "error lifting")
+            }
+          }
+        log.info("Raw response : " + responses.get().toString())
+        log.info("Request number : " + requestNo)
+
+        responses foreach {
+          case Seq(primaryResponse, candidateResponse, secondaryResponse) =>
+            liftRequest(req) respond {
+              case Return(m) =>
+                log.debug(s"success lifting request for ${m.endpoint}")
+
+              case Throw(t) => log.debug(t, "error lifting request")
+            } foreach { req =>
+              analyzer(req, candidateResponse, primaryResponse, secondaryResponse)
+            }
+        }
+
+        rawResponses = Future.Nil
+      }
+      requestNo = (requestNo + 1) % 3
       NoResponseExceptionFuture
     }
   }
